703207bafb9087e5ad979422cec2d2a4fa55d6d9
Bartłomiej Wysocki <bartekwysocki46@gmail.com>
2017-11-04 16:44:57 +0100

    Dodano okno rejestracji/logowania oraz pliki login oraz register php

---
+++ php/login.php
@@ -0,0 +1,11 @@
+<?php
+echo "Logowanie";
+echo "<br>";
+$login = $_POST['login'];
+$password = $_POST['password'];
+echo $login;
+echo "<br>";
+echo $password;
+?>
+
+
---
+++ php/register.php
@@ -0,0 +1,11 @@
+<?php
+echo "Rejestracja";
+echo "<br>";
+$login = $_POST['login'];
+$password = $_POST['password'];
+echo $login;
+echo "<br>";
+echo $password;
+?>
+
+
