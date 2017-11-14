bb201d50f863289993e7b503296f0bbbdffabbfd
kubiakdamian <kubiak.damian96@gmail.com>
2017-11-14 01:21:15 +0100

    SignIn połączony z phpem

--- src/session/SignIn.js
+++ src/session/SignIn.js
@@ -6,16 +6,17 @@ import Input from "../user-interface/Input";
 import Button from "../user-interface/Button";
 import Loader from "../user-interface/Loader";
 import { callToast } from "../lib/alert";
+import axios from "axios";
 var $ = require('jquery');
 
 export class SignIn extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { login: "", password: "", loader: false };
+    this.state = { login: "", password: "", loader: false,loginCheck: ""};
+
     this._handleSubmit = this._handleSubmit.bind(this);
     this._handleChange = this._handleChange.bind(this);
     this._handleChangeMsg = this._handleChangeMsg.bind(this);
-
   }
 
   updateLogin = e => {
@@ -64,25 +65,26 @@ export class SignIn extends React.Component {
     });
   }
 
-
-
   _handleSubmit(e){
   e.preventDefault();
 
   $.ajax({
       url: process.env.NODE_ENV !== "production" ? './index.php/Login/login' : "./index.php/Login/login",
       // url: "./php/mailer.php",
+      //./index.php/Login/login
       type: 'POST',
       data: {
         'login': this.state.login,
         'password': this.state.password
       },
       success: function(data) {
-        this.setState({
-          successMsg: '<div class="hehe"><h1>Wszystko GIT!</h1></div>'
-        });
-        $('#formContact').slideUp();
-        $('#formContact').after(this.state.successMsg);
+        this.state.loginCheck = data;
+      if(this.state.loginCheck=="exist"){
+        this.props.router.push("home_page");
+        }else if(this.state.loginCheck=="notExist"){
+      callToast("Entered login and password aren't correct!");
+      }
+
       }.bind(this),
       error: function(xhr, status, err) {
         console.log(xhr, status);
@@ -92,18 +94,11 @@ export class SignIn extends React.Component {
         });
       }.bind(this)
     });
-    if(this.state.login == "admin" && this.state.password == "admin"){
-      this.props.router.push("home_page");
-    }else{
-      callToast("Entered login and password aren't correct!");
-    }
   };
 
 
-
-
   render() {
-    return (
+    return (      
      <div className="row">
         <div className="col-md-12 " style={{ paddingTop: "30px" }}>
           <form onSubmit={this._handleSubmit}>
@@ -122,17 +117,14 @@ export class SignIn extends React.Component {
               placeholder="Password"
               type="password"
             />
-
             <ConfirmationButton
               onClick={event => {
                 this.onSubmit;
                 this.loaderUpdate();
-
               }}
               label={"Sign In"}
             />
           </form>
-
         </div>
       </div>
     );
