10e6ffbd7aa6e3297e1ab1372783a03b735fd0d0
Arkadiusz <arkadiusz1057@gmail.com>
2017-11-18 11:15:14 +0100

    Utworzenie rejestracji i haszowanie do bazy.

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,52 +3,50 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-    public function __construct() {
-        parent :: __construct();
-        $this->load->helper('url');
-        $this->load->model('User_model');
-    }
+	public function __construct() {
+		parent :: __construct();
+		$this->load->helper('url');
+		$this->load->model('User_model');
+	}
 
-    public function index() {
-        $this->load->view('login');
-    }
+	public function index() {
+		$this->load->view('login');
+	}
 
-    public function login() {
-        $user = new User_model();
-        $user->setLogin($this->input->post('login'));
-        $user->setPassword($this->input->post('password'));
-        $status="";
-        $user->checkLoginAndPassword();
-        if(!empty($user)) {
-            $status="exist";
-            header('Content-Type: application/json');
-            echo json_encode($status);
-        }
-        else {
-            $status="notExist";
-            header('Content-Type: application/json');
-            echo json_encode($status);
-        }
-    }
+	public function login() {
+		$login = $this->input->post('login');
+		$password = $this->input->post('password');
 
-    public function register() {
-        echo "Rejestracja";
-        echo "<br>";
-        $user = new User_model();
-        $user->setLogin($this->input->post('login'));
-        $user->setNick('nick');
-        $user->setPassword($this->input->post('password'));
-        $checkUnique = $user->checkUniqueLoginAndNick();
-        if($checkUnique != null) {
-            if($checkUnique == $user->getLogin()) {
-                $status = 'Login not unigue';
-            } else {
-                $status = 'Nick not unique';
-            }
-        }
-        else {
-            $user->save();
-        }
-    }
+		if($this->User_model->checkLoginAndPassword($login,$password)) {
+			$status="exist";
+		}
+		else {
+			$status="notExist";
+		}
+		header('Content-Type: application/json');
+		echo json_encode($status);
+	}
+
+	public function register() {
+		$user = new User_model();
+		$user->setLogin($this->input->post('login'));
+		$user->setNick('nock');
+		$user->setPassword($this->input->post('password'));
+		$checkUnique = $user->checkUniqueLoginAndNick();
+		if($checkUnique != null) {
+			if($checkUnique == $user->getLogin()) {
+				$status = 'Login not unigue';
+			} else {
+				$status = 'Nick not unique';
+			}
+		}
+		else {
+			$user->save();
+			$status = "notexist";
+		}
+		header('Content-Type: application/json');
+		echo json_encode($status);
+
+	}
 }
 ?>
