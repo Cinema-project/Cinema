764715b579bd94a99850d95f8c103371ce1503da
Bartłomiej Wysocki <bartekwysocki46@gmail.com>
2017-12-01 17:56:37 +0100

    Dodano dropdown kategorie i wyswietlanie w konsoli

--- src/FilmsPage/Films.js
+++ src/FilmsPage/Films.js
@@ -1,29 +1,54 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import apiClient from "../api-client";
+import { DropdownButton, MenuItem, Button } from "react-bootstrap";
 
-class Films extends Component{
-  constructor(props){
+class Films extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-
-    };
+      genres: [],
+      };
   }
 
   componentWillMount = () => {
     console.log("MOUNT");
     apiClient
-      .get("index.php/Home/getMovies")
+      .get("index.php/Home/getCategoryList/PL")
       .then(response => {
-        console.log("FILMY", response);
+        this.setState((state) => ({ genres: response.data.genres }))
       })
       .catch(error => {
         console.log(error);
       });
+
+
+
+
   };
 
-  render(){
-    return(
-      <div className = "row">
+  loadMovies = e => {
+    const category = e;
+    console.log(category);
+    apiClient
+      .get('index.php?/Home/getMovies/PL/'+category)
+      .then(response => {
+       console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="col-md-12" style={{ paddingTop: "5vh", paddingLeft: "5vh" }}>
+          <DropdownButton title= {"Wybierz kategorie"} onSelect={this.loadMovies}>
+            {this.state.genres.map((genre) => (
+              <MenuItem eventKey={genre.id}>{genre.name} </MenuItem>
+            ))}
+          </DropdownButton>
+        </div>
       </div>
     );
   }
