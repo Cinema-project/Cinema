0d0db1bd3b453132863e8f72c3917a1d5dc4f8bf
Bartłomiej Wysocki <bartekwysocki46@gmail.com>
2017-12-03 16:22:57 +0100

    Geolokalizacja-done

--- src/findCinemaPage/MapContainer.js
+++ src/findCinemaPage/MapContainer.js
@@ -1,18 +1,39 @@
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import React, {Component} from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component } from 'react';
 
 export class MapContainer extends Component {
-render() {
+  constructor(props) {
+    super(props);
+    this.state = { latitude: 0, longtitude: 0 }
+    //ustawiam pozycje za pomoca wbudowanej w przegladarke funkcji, nie dodaje na razie warunkow
+    //sprawdzajacych czy przegladraka obsluguje 
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        latitude: position.coords.latitude,
+        longtitude: position.coords.longitude
+      });
+    }
+    );
+  }
+  render() {
+    //dodaje ten warunek po to ze zanim latitude nie zmieni wartosci na konkretna lokalizacje to zwracam null
+    if (this.state.latitude == 0) {
+      return null;
+    }
     return (
-      <Map google={this.props.google} zoom={14}>
+
+      <Map google={this.props.google} initialCenter={{
+        lat: this.state.latitude,
+        lng: this.state.longtitude
+      }} zoom={14}>
 
         <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
+          name={'Current location'} />
 
         <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>"My Cinema"</h1>
-            </div>
+          <div>
+            <h1>"My Cinema"</h1>
+          </div>
         </InfoWindow>
       </Map>
     );
