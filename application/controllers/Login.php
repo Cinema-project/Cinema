5874f1bed8ea11eae29e48e0723a79fee882eee3
SharpShooter17 <b.ujazdowski@gmail.com>
2017-12-05 10:26:00 +0100

    Dodanie nagłówków

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+header('Access-Control-Allow-Origin: *');
 class Login extends CI_Controller {
 
     public function __construct() {
