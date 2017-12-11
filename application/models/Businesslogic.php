ccca5a08a9c18ff7027129fb86a67a64d75de92d
SharpShooter17 <b.ujazdowski@gmail.com>
2017-12-11 12:17:37 +0100

    Logika aplikacji

--- application/models/Businesslogic.php
+++ application/models/Businesslogic.php
@@ -3,6 +3,11 @@
 class BusinessLogic extends CI_Model{
   public function __construct(){
     parent::__construct();
+    $this->load->model('themoviedb');
+    $this->load->model('tmdbmovie_model', 'movies');
+  }
+  public function getMovies($categoryId, $page, $sort, $year){
+    
   }
 }
 
