5712e9426cc91c202fceec24d60f314848a6c687
Bartłomiej Wysocki <bartekwysocki46@gmail.com>
2017-12-11 19:16:58 +0100

    iss-19-markery-lokalizacja

--- src/findCinemaPage/MapContainer.js
+++ src/findCinemaPage/MapContainer.js
@@ -1,10 +1,13 @@
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import React, { Component } from 'react';
+import apiClient from "../api-client";
+import locationImage from "../images/location.png";
 
 export class MapContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = { latitude: 0, longtitude: 0 }
+    this.state = { latitude: 0, longtitude: 0,cinemaLocation: [] };
+    
     //ustawiam pozycje za pomoca wbudowanej w przegladarke funkcji, nie dodaje na razie warunkow
     //sprawdzajacych czy przegladraka obsluguje 
     navigator.geolocation.getCurrentPosition((position) => {
@@ -14,10 +17,24 @@ export class MapContainer extends Component {
       });
     }
     );
+    apiClient
+      .get("index.php/Home/getCinemasLocalization")
+      .then(response => {
+        this.setState((state)=>({cinemaLocation: response.data.result}))
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
   render() {
+    console.log(this.state.cinemaLocation);
+    
     //dodaje ten warunek po to ze zanim latitude nie zmieni wartosci na konkretna lokalizacje to zwracam null
     if (this.state.latitude == 0) {
+      return null;
+    }
+
+    if(this.state.cinemaLocation.length == 0) {
       return null;
     }
     return (
@@ -27,8 +44,15 @@ export class MapContainer extends Component {
         lng: this.state.longtitude
       }} zoom={14}>
 
-        <Marker onClick={this.onMarkerClick}
-          name={'Current location'} />
+        {this.state.cinemaLocation.map((clocation)=>(
+          
+           <Marker title={clocation.name}
+          icon={{
+          url:locationImage
+    }}
+         position={{lat: clocation.locationNS, lng: clocation.locationEW}} />
+        ))}
+       
 
         <InfoWindow onClose={this.onInfoWindowClose}>
           <div>
