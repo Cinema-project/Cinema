0a252fd8805515d6e6e7049ff26347c41b91ae1d
SharpShooter17 <b.ujazdowski@gmail.com>
2017-12-11 15:55:14 +0100

    Dodanie funkcji do kontrolera, która odpala inicjalizacje bazy geo

--- application/models/Cinemas_GeoCode_model.php
+++ application/models/Cinemas_GeoCode_model.php
@@ -1,6 +1,6 @@
 <?php
 
-class Cinemas_GeoCode_model extends CI_Model
+class Cinemas_geocode_model extends CI_Model
 {
 	public function insertDataToDataBase()
 	{
@@ -15,43 +15,43 @@ class Cinemas_GeoCode_model extends CI_Model
 		function getGeoCode($address)//zwraca tablice z szerokoscia i dlugoscia geograficzna podanego adresu
 		{
 			$addressCorrect = str_replace(' ','+',$address);
-			
-			
-			
+
+
+
 			$geocode = file_get_contents('https://maps.google.com/maps/api/geocode/json?address='.urlencode($addressCorrect).'&sensor=false');
-			
-			
-			
+
+
+
 			$output= json_decode($geocode, true);
-			
+
 			if($output['status']=="OK")//jesli znaleziono kordy
 			{
-			
+
 				$lat = $output['results'][0]['geometry']['location']['lat'];//szerokosc
 				$long = $output['results'][0]['geometry']['location']['lng'];//dlugosc
 				$addressResponse = $output['results'][0]['formatted_address'];
-				
+
 				if($lat && $long && $addressResponse)
 				{
 					//echo $addressResponse." ".$lat." ".$long;
 					//echo "\n";
 					//wstawiam dane do tablicy
-					$data_array = array();            
-					 
+					$data_array = array();
+
 					array_push
-					( 
+					(
 						$data_array,
-						$lat, 
+						$lat,
 						$long
 					);
-					 
+
 					return $data_array;
 				}
 				else
 				{
 					return false;
 				}
-				
+
 			}
 			else
 			{
@@ -66,10 +66,10 @@ class Cinemas_GeoCode_model extends CI_Model
 			$cinemaName = $child->cinema_name;
 			//echo $cinemaId;
 			//echo $cinemaName;
-			
+
 			array_push($cinemaNameArray, $cinemaName);
-			
-			
+
+
 		}
 		$cinemaNameResult = array_unique($cinemaNameArray);//tworze tablice bez powtorzonych rekordow
 		//dane konifguracyjne do polaczenia z baza danych
@@ -80,7 +80,7 @@ class Cinemas_GeoCode_model extends CI_Model
 		try
 		{
 			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
-			
+
 			if($polaczenie->connect_errno != 0)
 			{
 				throw new Exception(mysqli_connect_errno());
@@ -89,45 +89,45 @@ class Cinemas_GeoCode_model extends CI_Model
 			{
 				$zapytanieDelete = "delete from cinemas where id_cinema > 0";
 				$zapytanieResetAutoIncrement = "alter table cinemas auto_increment = 1";
-				
+
 				$result = $polaczenie->query($zapytanieDelete);
-					
+
 				if(!$result)
 				{
 					throw new Exception($polaczenie->error);
 				}
-				
+
 				$result = $polaczenie->query($zapytanieResetAutoIncrement);
-					
+
 				if(!$result)
 				{
 					throw new Exception($polaczenie->error);
 				}
-				
-				
+
+
 				foreach($cinemaNameResult as $cinemaName)
 				{
 					$address = $cinemaName;
 					$dataArray = array();
 					$dataArray = getGeoCode($address);
-					
+
 					if($dataArray)
 					{
 						$lat = $dataArray[0];
 						$long = $dataArray[1];
-						
+
 						try
 						{
 							$zapytanie = "insert into cinemas (name, locationEW, locationNS)
 							values ('$cinemaName','$long', '$lat')
-							
-							
-							
+
+
+
 							";
-						
-						
+
+
 							$result = $polaczenie->query($zapytanie);
-							
+
 							if(!$result)
 							{
 								throw new Exception($polaczenie->error);
@@ -138,11 +138,11 @@ class Cinemas_GeoCode_model extends CI_Model
 						}
 					}
 				}
-				
+
 				$polaczenie->close();
 			}
-				
-				
+
+
 		}catch(Exception $e)
 		{
 			echo 'Blad serwera i polaczenia z baza danych'.$e;
