0825058490463fe1d3eef459ec513f5e5f2c0ffe
SharpShooter17 <b.ujazdowski@gmail.com>
2017-12-11 16:25:08 +0100

    Funkcje do zaimplementowania

--- application/models/Businesslogic.php
+++ application/models/Businesslogic.php
@@ -6,8 +6,23 @@ class BusinessLogic extends CI_Model{
     $this->load->model('themoviedb');
     $this->load->model('tmdbmovie_model', 'movies');
   }
+
   public function getMovies($categoryId, $page, $sort, $year){
-    
+  }
+  public function getCategoryList(){
+  }
+  public function getMovieDetails($id){
+  }
+  public function getLastest(){
+  }
+  public function getNowPlaying($page, $region){
+  }
+  public function getPopular($page, $region){
+  }
+  public function getTopRated($page, $region){
+  }
+  public function getUpcoming($page, $region){
+
   }
 }
 
