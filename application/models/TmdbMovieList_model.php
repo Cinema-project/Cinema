fd6fba68a0deebc0afc05036f6df778b584a78db
Mateusz Sędkowski <mateusz.sedkowski@gmail.com>
2017-12-17 19:07:50 +0100

    Create new model with select by genre and select by time function

---
+++ application/models/TmdbMovieList_model.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mateusz
+ * Date: 17.12.2017
+ * Time: 12:59
+ */
+
+class TmdbMovieList_model extends CI_Model
+{
+    private $movieList = array();
+
+    /**
+     * @return array
+     */
+    public function getMovieList()
+    {
+        return $this->movieList;
+    }
+
+    /**
+     * @param array $movieList
+     */
+    public function setMovieList($movieList)
+    {
+        $this->movieList = $movieList;
+    }
+
+    public function selectByGenre($genreName = null, $sortBy = null)
+    {
+        if($genreName != null)
+        {
+            $sql = "SELECT id_genre FROM genres WHERE name = ?";
+            $genreId = $this->db->query($sql, $genreName)->result_array()[0];
+            if($genreId != null)
+            {
+                if($sortBy == null)
+                {
+                    $sql = "SELECT * FROM tmdbmovies JOIN genres_movie ON tmdbmovies.MovieID = genres_movie.id_movie WHERE id_genre = ? ";
+                    $movies = $this->db->query($sql, $genreId)->result_array();
+                }
+                else
+                {
+                    $sql = "SELECT * FROM tmdbmovies JOIN genres_movie ON tmdbmovies.MovieID = genres_movie.id_movie WHERE id_genre = ? ORDER BY ?";
+                    $movies = $this->db->query($sql, $genreId, $sortBy)->result_array();
+                }
+                foreach ($movies as $movie) {
+                    $movieModel = new Tmdbmovie_model();
+                    $movieModel->setTmdbMovie($movie);
+                    $this->movieList[] = $movieModel;
+                }
+            }
+        }
+    }
+
+    public function selectByTime($startTime = null, $endTime = null, $sortBy = null)
+    {
+        if($startTime != null && $endTime != null)
+        {
+            if($sortBy == null)
+            {
+                $sql = "SELECT * FROM tmdbmovies WHERE Premiere_date BETWEEN ? AND ?";
+                $movies = $this->db->query($sql, $startTime, $endTime)->result_array();
+            }
+            else {
+                $sql = "SELECT * FROM tmdbmovies WHERE Premiere_date BETWEEN ? AND ? ORDER BY ?";
+                $movies = $this->db->query($sql, $startTime, $endTime, $sortBy)->result_array();
+            }
+            if($movies != null)
+            {
+                foreach ($movies as $movie) {
+                    $movieModel = new Tmdbmovie_model();
+                    $movieModel->setTmdbMovie($movie);
+                    $this->movieList[] = $movieModel;
+                }
+            }
+        }
+    }
+
+}
