244b27720ff447c4fd62fc0942009eb11970ef8e
SharpShooter <b.ujazdowski@gmail.com>
2018-01-02 11:15:13 +0100

    'done'

--- application/models/Update_model.php
+++ application/models/Update_model.php
@@ -192,12 +192,15 @@ class Update_model extends CI_Model {
      */
     public function updateCinemaRepertoire($repertoire){
       if ($this->c->checkIfUpdate('CINEMA_REPERTOIRE_UPDATE', $repertoire['created'])){
+        $this->event->removeOldEvent();
         return 'Repertoire is up to date';
       }
 
-      ini_set('max_execution_time', 300);
+      ini_set('max_execution_time', 0);
 
       $this->load->model('event_model', 'event');
+      $this->db->truncate('events');
+
       foreach ($repertoire['movies'] as $event) {
         $this->event->setTime($event['time']);
         $this->event->setIdMovie($event['movieId']);
