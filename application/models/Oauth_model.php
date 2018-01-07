4a4c00c30aec4adb561f8558e92e10f5b0265521
SharpShooter <b.ujazdowski@gmail.com>
2018-01-07 20:34:20 +0100

    dziala na sesji

--- application/models/Oauth_model.php
+++ application/models/Oauth_model.php
@@ -4,11 +4,18 @@ require_once('Facebook/autoload.php');
 
 class Oauth_model extends CI_Model {
 
+  private $appId = '336960786712721';
+  private $appSecret = '7e90ee09172c91422bda26408d62d590';
+
   public function login(){
+    if (!session_id()) {
+    session_start();
+}
     $fb = new Facebook\Facebook([
-      'app_id' => '336960786712721', // Replace {app-id} with your app id
-      'app_secret' => '7e90ee09172c91422bda26408d62d590',
+      'app_id' => $this->appId, // Replace {app-id} with your app id
+      'app_secret' => $this->appSecret,
       'default_graph_version' => 'v2.2',
+      'persistent_data_handler'=>'session'
       ]);
 
     $helper = $fb->getRedirectLoginHelper();
@@ -20,10 +27,14 @@ class Oauth_model extends CI_Model {
   }
 
   public function callback(){
+    if (!session_id()) {
+    session_start();
+}
     $fb = new Facebook\Facebook([
-      'app_id' => '336960786712721', // Replace {app-id} with your app id
-      'app_secret' => '7e90ee09172c91422bda26408d62d590',
+      'app_id' => $this->appId, // Replace {app-id} with your app id
+      'app_secret' => $this->appSecret,
       'default_graph_version' => 'v2.2',
+      'persistent_data_handler'=>'session'
       ]);
 
     $helper = $fb->getRedirectLoginHelper();
@@ -67,7 +78,7 @@ class Oauth_model extends CI_Model {
     var_dump($tokenMetadata);
 
     // Validation (these will throw FacebookSDKException's when they fail)
-    $tokenMetadata->validateAppId('{app-id}'); // Replace {app-id} with your app id
+    $tokenMetadata->validateAppId($this->appId); // Replace {app-id} with your app id
     // If you know the user ID this access token belongs to, you can validate it here
     //$tokenMetadata->validateUserId('123');
     $tokenMetadata->validateExpiration();
