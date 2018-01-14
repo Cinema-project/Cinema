dd9c7cdd68c8cb51cb6abfd6a57f4fdd0c6d69e0
kubiakdamian <kubiak.damian96@gmail.com>
2018-01-14 01:49:09 +0100

    Utworzony widok tła dla filmu w zakładce ulubionych

--- src/FavouritesPage/Favourites.js
+++ src/FavouritesPage/Favourites.js
@@ -1,13 +1,17 @@
 import React, {Component} from "react";
 import apiClient from "../api-client";
 import { connect } from "react-redux";
+import styled from "styled-components"
+import ReactImageFallback from "react-image-fallback"
+import loaderImage from "../images/loader.GIF"
 var $ = require('jquery');
 
 class Favourites extends Component{
   constructor(props){
     super(props);
     this.state = {
-      favourites: []
+      favourites: [],
+      backs: []
     }
   }
 
@@ -19,6 +23,7 @@ class Favourites extends Component{
           'token' : this.props.user.token,
         },
         success: function(data) {
+          console.log("DATA FAV", data);
           this.setState({
             favourites: data.movies
           })
@@ -34,11 +39,19 @@ class Favourites extends Component{
   }
 
   render(){
-    console.log("FAV PAGE:", this.state.favourites);
     return(
-      <div className = "row">
+      <div>
+        {this.state.favourites.map(favourite=>
+          <Film className="col-md-8 col-md-offset-2" style={{marginTop: "10vh"}}>
+            <Title>{favourite.title}</Title>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${favourite.screen}`}
+              style={{ width: "920px", height: "400px", borderRadius: "10px", opacity: "0.3"}}
+            />
+          </Film>
+        )}
       </div>
-    );
+    )
   }
 }
 
@@ -49,3 +62,42 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps)(Favourites);
+
+const Poster = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 4;
+
+  &:hover{
+    cursor: pointer;
+  }
+`
+
+const Film = styled.div`
+  position: relative;
+`
+
+const Title = styled.div`
+  width: 100%;
+  position: absolute;
+  opacity: 1;
+  color: white;
+  text-shadow: 2px 2px 10px rgba(150, 150, 150, 1);
+  font-size: 45px;
+  text-align: center;
+`
+
+const Description = styled.div`
+  text-align: center;
+  font-size: 15px;
+  color: white;
+  margin-top: 4vh;
+`
+
+const Rating = styled.div`
+  text-align: center;
+  font-size: 30px;
+  color: white;
+  padding-top: 12vh;
+`
